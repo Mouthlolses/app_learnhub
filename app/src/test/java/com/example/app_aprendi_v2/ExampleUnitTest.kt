c6532da53630565e8729754f8cae084580f75f47
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2024-10-24 23:41:04 -0300

    alterations2

--- app/src/test/java/com/example/app_aprendi_v2/ExampleUnitTest.kt
+++ app/src/test/java/com/example/app_aprendi_v2/ExampleUnitTest.kt
@@ -3,24 +3,15 @@ package com.example.app_aprendi_v2
 import org.junit.Test
 
 import org.junit.Assert.*
-import org.junit.Rule
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 
-
-class BottomAppBarContentTest {
-
-    @get:Rule
-    val composeTestRule = createComposeRule()
-
-}
 class ExampleUnitTest {
     @Test
-    fun Home() {
+    fun home() {
         assertEquals(4, 2 + 2)
     }
 }
